71282f74ec45ea82e8b8eec2b7b6687197c56f41
duanjianyao <1907055509@qq.com>
2022-06-16 20:15:10 +0800

    master test commit

--- src/main/java/com/atguigu/Test.java
+++ src/main/java/com/atguigu/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("hello git");
         System.out.println("hello 2");
         System.out.println("hello hot-fix");
+        System.out.println("hello master");
     }
 }
